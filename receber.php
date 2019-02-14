3e622a60594972e18e5704fa238129786f540749
Franciel <frannciel.edu@gmail.com>
2019-02-14 21:17:58 +0000

    Criando tabela

--- receber.php
+++ receber.php
@@ -7,30 +7,48 @@ $colunas 	    = getPost('colunas');
 $linhas = explode($Separalinha, $texto);
 foreach ($linhas as $linha){
     $colunas = explode($SeparaColuna, $linha);
-    vardump($colunas);
+    var_dump($colunas);
 }
 
 function getPost($valor) {
     return isset($_POST[$valor]) ? $_POST[$valor] : '';
 }
 
+function setHeader($valor){
+   for ($i = 1; $i <= $valor; $i++){
+       $colunas += '<th>Coluna'.$valor.'</th>';
+    }
+    return $colunas;
+}
+
+function setColumn($valor){
+    return <<<EOD
+        <td>$valor</td>
+EOD;
+}
+
+function setRow($valor){
+    return <<<EOD
+        <tr>$valor</tr>
+EOD;
+}
+
 function getOpcao($numero){
-    switch(){
-        case: 0
+    switch($numero){
+        case '0':
         return ',';
-        case: 1
+        case '1':
         return '-';
-        case: 2
+        case '2':
         return '/';
-        case: 3
+        case '3':
         return chr(32);
-        case: 4
+        case '4':
         return '    ';
-        case: 5
+        case '5':
         return '.';
-        case: 6
+        case '6':
         return ';';
     }
-
 }
 ?>
