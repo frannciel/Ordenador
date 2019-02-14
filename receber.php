b7236827df2185e3f29ce23f1a94eaba3bc2c452
Franciel <frannciel.edu@gmail.com>
2019-02-14 20:28:07 +0000

    testes

--- receber.php
+++ receber.php
@@ -1,7 +1,7 @@
 <?php
-$Separalinha  	= getPost('slinha');
+$Separalinha  	= getOpcao(getPost('sLinha'));
 $texto		    = getPost('texto');
-$SeparaColuna   = getPost('SColuna');
+$SeparaColuna   = getOpcao(getPost('sColuna'));
 $colunas 	    = getPost('colunas');
 
 $linhas = explode($Separalinha, $texto);
@@ -12,5 +12,25 @@ foreach ($linhas as $linha){
 
 function getPost($valor) {
     return isset($_POST[$valor]) ? $_POST[$valor] : '';
+}
+
+function getOpcao($numero){
+    switch(){
+        case: 0
+        return ',';
+        case: 1
+        return '-';
+        case: 2
+        return '/';
+        case: 3
+        return chr(32);
+        case: 4
+        return '    ';
+        case: 5
+        return '.';
+        case: 6
+        return ';';
+    }
+
 }
 ?>
