66f4a0e32ffc8c98d418728dca1686220e9aaa26
Franciel <frannciel.edu@gmail.com>
2019-02-14 19:45:07 +0000

    corrigindo

--- receber.php
+++ receber.php
@@ -1,9 +1,11 @@
 <?php
+$Separalinha  	= getFile('slinha');
+$texto		    = getFile('texto');
+$SeparaColuna   = getFile('SColuna');
+$colunas 	    = getFile('colunas');
 
 $linhas = explode($Separalinha, $texto);
-
 foreach ($linhas as $linha){
-
     $colunas = explode($SeparaColuna, $linha);
     vardump($colunas);
 }
